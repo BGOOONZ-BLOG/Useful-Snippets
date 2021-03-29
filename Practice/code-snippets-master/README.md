0cf2a0d9492ca4ab766beb7d613fa699e83fb015
Bryan C Guner <bryan.guner@gmail.com>
2021-03-29 07:22:32 +0000

    trimmed fat and added web utils

--- Practice/code-snippets-master/README.md
+++
@@ -1,4 +0,0 @@
-# code-snippets
-
-This is a personal repository of useful helper functions that may become useful in
-projects or in understanding how to approach problems in the future.
--- Practice/code-snippets-master/memoization-tabulation/fibonacci-versions/naive-fibonacci.js
+++
@@ -1,6 +0,0 @@
-let fib = (n) => (n === 1 || n === 2 ? 1 : fib(n - 1) + fib(n - 2));
-
-// let fibonacci = (40) => {
-//   if (n === 1 || n === 2) return 1;
-//   return fibonacci(n - 1) + fibonacci(n - 2);
-// };
