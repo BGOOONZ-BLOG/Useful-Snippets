b71882433b1d158c4daa84d00304a5793d250ec4
bryan <bryan.guner@gmail.com>
2021-11-28 21:51:26 -0500

    wubalubadubdub

--- arrays/09-arrowFunctions.js
+++ arrays/09-arrowFunctions.js
@@ -1,10 +1,10 @@
 // syntax
-(param1, param2, …, paramN) => { statements }
-(param1, param2, …, paramN) => expression
+(param1, param2, ... paramN) => { statements }
+(param1, param2, ... paramN) => expression
 // equivalent to: => { return expression; }
 
 // Parentheses are optional when there's only one parameter name:
-(singleParam) => { statements }
+    ( singleParam ) => { statements };
 singleParam => { statements }
 
 // The parameter list for a function with no parameters should be written with a pair of parentheses.
