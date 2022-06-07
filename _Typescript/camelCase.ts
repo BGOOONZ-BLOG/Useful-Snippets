175a56419f4650f1f89fd0a92532b626645fc1ec
Bryan C Guner <bryan.guner@gmail.com>
2022-06-07 16:47:24 +0000

    -update

---
+++ _Typescript/camelCase.ts
@@ -0,0 +1,9 @@
+export default function toCamelCase(string: string) {
+  const result = string
+    .toLowerCase()
+    .trim()
+    .split(/[ -_]/g)
+    .map(word => word.replace(word[0], word[0].toString().toUpperCase()))
+    .join('');
+  return result.replace(result[0], result[0].toLowerCase());
+}
